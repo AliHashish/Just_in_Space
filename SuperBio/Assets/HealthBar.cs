e941aea8401ef1fcd5b566bbff224c8f51c0892d
AliHashish <67716981+AliHashish@users.noreply.github.com>
2022-09-30 17:46:51 +0200

    some modifications
    b3mlha push 3lshan el laptop m3ala2 w 5ayef y2fl fag2a xDDDD

--- SuperBio/Assets/HealthBar.cs
+++ SuperBio/Assets/HealthBar.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
+    public PlayerVars playerHealth;
+    public Image fillImage;
+    private Slider slider;
     // Start is called before the first frame update
     void Start()
     {
-        
+        slider = GetComponent<Slider>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (slider.value <= slider.minValue)    // byetfy el fill image lama el health yewsal 0
+        {
+            fillImage.enabled = false;
+        }
+        else
+        {
+            fillImage.enabled = true;
+        }
+        float fillValue = playerHealth.health / playerHealth.maxHealth;
+        slider.value = fillValue;
     }
 }
--- SuperBio/Assets/PlayerVars.cs
+++ SuperBio/Assets/PlayerVars.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class PlayerVars : MonoBehaviour
 {
+    public float health = 100f;
+    public float maxHealth = 100f;
+    public Vector3 respawnPoint;       // where to spawn upon death
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        respawnPoint = transform.position;
+        health = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (health <= 0)
+        {
+            if (gameObject.CompareTag("Player"))        // player respawn upon death
+            {
+                health = maxHealth;
+                transform.position = respawnPoint;      //  yt3mlo respawn fy 7eta
+            }
+        }
     }
 }
